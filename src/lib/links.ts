690e1f8f03d85cee26a6484616b8f8941036240e
Ryunosuke Yoda <147054812+takoyakidath@users.noreply.github.com>
2026-08-08 15:07:31 +0900

    Update links.ts

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -14,28 +14,28 @@ export const profileData = {
 
 export const links: Link[] = [
   {
-    id: 1,
+    id: 2,
     title: "Instagram",
     content: "@takoyakidath",
     url: "https://instagram.com/takoyakidath",
     icon: "instagram",
   },
   {
-    id: 2 ,
+    id: 3 ,
     title: "Discord",
     content: "@takoyaki_dath",
     url: "https://discord.com/users/1151099184691302421",
     icon: "discord",
   },
   {
-    id: 3,
+    id: 4,
     title: "GitHub",
     content: "@takoyakidath",
     url: "https://github.com/takoyakidath",
     icon: "github",
   },
   {
-    id: 4,
+    id: 1,
     title: "Website",
     content: "My personal blog",
     url: "https://takoyaki.pkopko.jp",
